6f35e0739b6788f8779e4a25a121468a1928dc81
Esteban Flores <Esteban.Flores@endava.com>
2019-08-02 10:55:01 -0500

    Lesson 2

--- app/src/main/java/com/ejfr1985/smack/MainActivity.kt
+++ app/src/main/java/com/ejfr1985/smack/MainActivity.kt
@@ -11,6 +11,7 @@ import android.support.design.widget.NavigationView
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
+import android.view.View
 
 class MainActivity : AppCompatActivity(){
 
@@ -39,6 +40,19 @@ class MainActivity : AppCompatActivity(){
         } else {
             super.onBackPressed()
         }
+    }
+
+    fun addChannelBtnClicked(view: View){
+
+    }
+
+    fun loginNavBarClicked(view: View){
+
+    }
+
+    fun sendMsgBtnClicked(view: View){
+
+
     }
 
 }
