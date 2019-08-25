9390e57d642f23ae1d95717fb070f5ff73485d29
Esteban Flores <ejfr1985@gmail.com>
2019-08-25 10:54:10 -0500

    lesson 4

---
+++ app/src/main/java/com/ejfr1985/smack/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.ejfr1985.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun createAvatarImageViewClicked(view: View){
+
+    }
+
+    fun createGenerateBackgroundBtnClicked(view: View){
+
+    }
+
+    fun createUserBtnClicked(view: View){
+
+    }
+}
