3b10a2e10e88d9d5397c4b4735054d53b4a1183b
Esteban Flores <ejfr1985@gmail.com>
2019-10-03 06:51:57 -0500

    lesson 12

--- app/src/main/java/com/ejfr1985/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/ejfr1985/smack/Controller/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.ejfr1985.smack.Controller
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.ejfr1985.smack.R
 import com.ejfr1985.smack.Services.AuthService
 import kotlinx.android.synthetic.main.activity_login.*
@@ -13,27 +16,67 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginSpinner.visibility = View.INVISIBLE
     }
 
     fun loginLoginBtnClicked(view: View) {
 
         val email = loginEmailText.text.toString()
         val password = loginPasswordText.text.toString()
+        enableSpinner(true)
+        inputManager()
+        if (email.isNotEmpty() && password.isNotEmpty()) {
 
-        AuthService.loginUser(this, email, password) { loginSucces ->
-            if (loginSucces) {
-                AuthService.findUserByEmail(this) { findSuccess ->
-                    if (findSuccess) {
-                        finish()
+            AuthService.loginUser(this, email, password) { loginSuccess ->
+                if (loginSuccess) {
+                    AuthService.findUserByEmail(this) { findSuccess ->
+                        if (findSuccess) {
+                            enableSpinner(false)
+                            finish()
+                        } else {
+                            errorToast("Error while retrieving user information")
+                        }
                     }
+                } else {
+                    errorToast("Error while logging in")
                 }
             }
+
+        } else {
+            errorToast("Please, fill in both email and password fields")
         }
+
     }
 
     fun loginCreateUserBtnClicked(view: View) {
         val createUserIntent = Intent(this, CreateUserActivity::class.java)
         startActivity(createUserIntent)
         finish()
+    }
+
+
+    fun errorToast(msg: String) {
+        enableSpinner(false)
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+
+    fun enableSpinner(enable: Boolean) {
+
+        if (enable) {
+            loginSpinner.visibility = View.VISIBLE
+        } else {
+            loginSpinner.visibility = View.INVISIBLE
+        }
+
+        loginLoginBtn.isEnabled = !enable
+        loginCreateUserBtn.isEnabled = !enable
+
+    }
+
+    fun inputManager() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
     }
 }
